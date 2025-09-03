d64765f584a397b17f389f40d83221b1c2134521
just_a_sapien <victorysharma07@gmail.com>
2025-09-03 12:34:06 +0530

    minor updates in frontend

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -11,7 +11,7 @@ axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 const WriteArticle = () => {
   const articleLength = [
     { length: 800, text: "Short (500-800 words)" },
-    { length: 1200, text: "Short (800-1200 words)" },
+    { length: 1200, text: "Medium (800-1200 words)" },
     { length: 1600, text: "Long (1200+ words)" },
   ];
 
